f8779f1b3bad260d28a8d9b37fdea898abd775b4
Crandel <cradlemann@gmail.com>
2024-11-14 18:08:34 +0100

    Review fixes

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,7 @@ import (
 
 func main() {
 	ctx := context.Background()
+
 	debug := env.GetEnv("DEBUG", "false")
 	logLevel := slog.LevelInfo
 	showSources := false
@@ -23,33 +24,34 @@ func main() {
 		showSources = true
 		logLevel = slog.LevelDebug
 	}
-
 	logging.InitLogger(logLevel, showSources)
+
 	addUserFlag := flag.Bool("add", false, "Add new user to specific mail client")
+	systemKeyringFlag := flag.Bool("system_keyring", false, "Use system keyring instead of file")
 
 	flag.Parse()
 
 	var err error
 	if *addUserFlag {
 		err = config.AddToConfig()
-		slog.Error("failed to add user to config", slog.Any("error", err))
+		slog.ErrorContext(ctx, "failed to add user to config", slog.Any("error", err))
 		return
 	}
-	systemKeyringFlag := flag.Bool("system_keyring", false, "Use system keyring instead of file")
-	flag.Parse()
 
-	channel := make(chan string)
-	defer close(channel)
 	file, err := config.GetFile()
 	if err != nil {
-		slog.Error("error during get file", slog.Any("error", err))
+		slog.ErrorContext(ctx, "error during get file", slog.Any("error", err))
 		return
 	}
 	listAccounts, err := config.GetAccounts(file)
 	if err != nil {
-		slog.Error("unable to retrieve account list")
+		slog.ErrorContext(ctx, "unable to retrieve account list")
 		return
 	}
+
+	channel := make(chan string)
+	defer close(channel)
+
 	for _, acc := range listAccounts {
 		if acc.MailType == accounts.Gmail {
 			if googlemail.CheckOnline() {
